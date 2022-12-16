a2318554e9fb773eea6dca563abb8a1b3cfd3dcc
[subimm] <[subimm630@gmail.com]>
2022-12-16 15:11:28 +0900

    initial

---
+++ src/main/java/com/codestates/controller/TodoController.java
@@ -0,0 +1,13 @@
+package com.codestates.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "main branch 실습";
+    }
+}
